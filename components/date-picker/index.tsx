8a3d8cec0016e41558cb55e0ffe747dbc24da3ea
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2024-10-09 17:37:32 -0300

    fix(date-picker): changes popover to correct component

--- components/date-picker/index.tsx
+++ components/date-picker/index.tsx
@@ -2,21 +2,16 @@
 
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/button";
 import { Calendar } from "@/components/calendar";
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover } from "@/components/popover";
 import { DatePickerProps } from "./types";
 
 export const DatePicker = ({ value, onChange }: DatePickerProps) => {
 	return (
-		<Popover>
-			<PopoverTrigger asChild>
+		<Popover.Root>
+			<Popover.Trigger asChild>
 				<Button
 					variant="outline"
 					className={cn(
@@ -27,15 +22,15 @@ export const DatePicker = ({ value, onChange }: DatePickerProps) => {
 					<CalendarIcon className="mr-2 size-4" />
 					{value ? format(value, "PPP") : <span>Pick a date</span>}
 				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-auto p-0">
+			</Popover.Trigger>
+			<Popover.Content className="w-auto p-0">
 				<Calendar
 					mode="single"
 					selected={value}
 					onSelect={onChange}
 					initialFocus
 				/>
-			</PopoverContent>
-		</Popover>
+			</Popover.Content>
+		</Popover.Root>
 	);
 };
