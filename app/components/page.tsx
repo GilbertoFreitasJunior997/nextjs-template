599f9fa28e4346596b26f87671b8bda1cb3cc589
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2024-10-09 11:33:10 -0300

    feat: adds accordion component

---
+++ app/components/page.tsx
@@ -0,0 +1,12 @@
+import { AccordionSample } from "./_samples/accordion-sample";
+
+export default function ComponentsPage() {
+	return (
+		<div>
+			Components
+			<div>
+				<AccordionSample />
+			</div>
+		</div>
+	);
+}
