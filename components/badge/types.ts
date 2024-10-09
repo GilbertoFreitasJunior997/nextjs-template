b16d92d7c45c7ab76865ff795ecaeee27d9eef7c
lucas.dalan <lucasdalan09@gmail.com>
2024-10-09 11:29:50 -0300

    feat(badge): adiciona componente badge

---
+++ components/badge/types.ts
@@ -0,0 +1,5 @@
+import { VariantProps } from "class-variance-authority";
+import { HTMLAttributes } from "react";
+import { badgeVariants } from "./consts";
+
+  export type BadgeProps = HTMLAttributes<HTMLDivElement> & VariantProps<typeof badgeVariants>
