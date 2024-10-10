69264cd3597f2adabbd7a70fe2cb19490ca279b0
lucas.dalan <lucasdalan09@gmail.com>
2024-10-09 21:46:29 -0300

    feat(sonner): adds sonner component

---
+++ app/components/_samples/sonner-sample/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Button } from "@/components/button";
+import { toast } from "sonner";
+
+export const SonnerSample = () => {
+	const handleClick = () => {
+		toast("I am an example");
+	};
+	return (
+		<div>
+			<Button onClick={handleClick}>Sonner Example</Button>
+		</div>
+	);
+};
