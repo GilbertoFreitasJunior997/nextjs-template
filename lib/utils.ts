b0ab15b48715fc3889ff4dcee47c2566c33e83f5
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2024-10-14 14:01:00 -0300

    fix(form): uppercase first name letter if no label provided

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
+
+export const uppercaseFirstLetter = (str: string) =>
+  `${str.substring(0, 1).toUpperCase()}${str.substring(1)}`;
