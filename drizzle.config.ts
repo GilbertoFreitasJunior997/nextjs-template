d4b9674b2fb041e9e4ad6b65503c7519d7f51b58
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2024-10-17 16:40:05 -0300

    refactor: migrate to src directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
 	dialect: "postgresql",
-	schema: "./db/schemas/*",
-	out: "./db/drizzle",
+	schema: "./src/db/schemas/*",
+	out: "./src/db/drizzle",
 	dbCredentials: { url: process.env.DATABASE_URL ?? "" },
 });
