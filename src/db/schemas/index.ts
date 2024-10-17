abb54328643d63ce32013501dac2356d814973e1
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2024-10-17 16:49:05 -0300

    refactor: applies biome on new src dir

--- src/db/schemas/index.ts
+++ src/db/schemas/index.ts
@@ -1,2 +1,3 @@
+// biome-ignore lint/performance/noBarrelFile: <explanation>
 export * from "./sessions-schema";
 export * from "./users-schema";
--- src/services/_base/index.ts
+++ src/services/_base/index.ts
@@ -3,45 +3,45 @@ import { eq } from "drizzle-orm";
 import { BaseService, BaseTable } from "./types";
 
 export const createService = <
-	TModel extends object,
-	TInsert extends object,
-	TId extends number | string = number,
+  TModel extends object,
+  TInsert extends object,
+  TId extends number | string = number,
 >(
-	table: BaseTable,
+  table: BaseTable,
 ): BaseService<TModel, TInsert, TId> => {
-	return {
-		create: async (data: TInsert) => {
-			const [newRecord] = await db.insert(table).values(data).returning();
+  return {
+    create: async (data: TInsert) => {
+      const [newRecord] = await db.insert(table).values(data).returning();
 
-			return newRecord as TModel;
-		},
-		get: async () => {
-			const data = await db.select().from(table).execute();
+      return newRecord as TModel;
+    },
+    get: async () => {
+      const data = await db.select().from(table).execute();
 
-			return data as TModel[];
-		},
-		getById: async (id: TId) => {
-			const [data] = await db
-				.select()
-				.from(table)
-				.where(eq(table.id, id))
-				.limit(1);
+      return data as TModel[];
+    },
+    getById: async (id: TId) => {
+      const [data] = await db
+        .select()
+        .from(table)
+        .where(eq(table.id, id))
+        .limit(1);
 
-			return data as TModel;
-		},
-		update: async (id: TId, data: Partial<TInsert>) => {
-			const [updatedData] = await db
-				.update(table)
-				.set(data)
-				.where(eq(table.id, id))
-				.returning();
+      return data as TModel;
+    },
+    update: async (id: TId, data: Partial<TInsert>) => {
+      const [updatedData] = await db
+        .update(table)
+        .set(data)
+        .where(eq(table.id, id))
+        .returning();
 
-			return updatedData as TModel;
-		},
-		delete: async (id: TId) => {
-			const data = await db.delete(table).where(eq(table.id, id)).returning();
+      return updatedData as TModel;
+    },
+    delete: async (id: TId) => {
+      const data = await db.delete(table).where(eq(table.id, id)).returning();
 
-			return data as TModel;
-		},
-	};
+      return data as TModel;
+    },
+  };
 };
