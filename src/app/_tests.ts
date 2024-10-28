d733b1a1482365dd657ebc2088ee25386cadc126
lucas.dalan <lucasdalan09@gmail.com>
2024-10-28 15:41:26 -0300

    feat(lucia): lib/session.ts

--- src/app/_tests.ts
+++ src/app/_tests.ts
@@ -2,9 +2,6 @@
 
 import { sessionService } from "@/services/session";
 import { userService } from "@/services/user";
-import { cookies } from "next/headers";
-import { cache } from "react";
-import "server-only";
 
 export const createUser = async () => {
   const user = await userService.create({
@@ -13,35 +10,6 @@ export const createUser = async () => {
 
   return user;
 };
-
-export async function setSessionTokenCookie(token: string, expiresAt: Date) {
-  cookies().set("session", token, {
-    httpOnly: true,
-    sameSite: "lax",
-    secure: process.env.NODE_ENV === "production",
-    expires: expiresAt,
-    path: "/",
-  });
-}
-
-export async function deleteSessionTokenCookie() {
-  cookies().set("session", "", {
-    httpOnly: true,
-    sameSite: "lax",
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 0,
-    path: "/",
-  });
-}
-
-export const getCurrentSession = cache(async () => {
-  const token = cookies().get("session")?.value ?? null;
-  if (token === null) {
-    return { session: null, user: null };
-  }
-  const result = await sessionService.validateSessionToken(token);
-  return result;
-});
 
 export const login = async () => {
   const token = sessionService.generateSessionToken();
--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -2,4 +2,4 @@ import { usersTable } from "@/db/schemas";
 import { User, UserInsert } from "@/models/user-model";
 import { createService } from "../_base";
 
-export const userService = createService<User, UserInsert, string>(usersTable);
+export const userService = createService<User, UserInsert>(usersTable);
